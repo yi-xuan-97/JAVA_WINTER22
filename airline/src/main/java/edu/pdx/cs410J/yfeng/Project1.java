478aea871a8188699a43fe18fe335e60162f457b
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2022-01-31 17:12:04 -0800

    resubmit project 1

---
+++ airline/src/main/java/edu/pdx/cs410J/yfeng/Project1.java
@@ -0,0 +1,11 @@
+package edu.pdx.cs410J.yfeng;
+
+import edu.pdx.cs410J.ParserException;
+
+import java.io.IOException;
+
+public class Project1 {
+    public static void main(String[] args) throws ParserException, IOException {
+        Project2.main(args);
+    }
+}
